4dfa446459dcab78023ca076c5ac621d21aacb34
Spencer Haggard <spencer.timekeeper99@gmail.com>
2018-01-11 16:47:19 -0500

    base game

---
+++ sketch.js
@@ -0,0 +1,82 @@
+// Minesweeper
+
+let grid;
+let cols;
+let rows;
+let w = 20;
+
+let totalBees = 100;
+
+function make2DArray(cols, rows) {
+	let arr = new Array(cols);
+	for (let i = 0; i < arr.length; i++) {
+		arr[i] = new Array(rows);
+	}
+	return arr;
+}
+
+function setup() {
+	createCanvas(401, 401);
+
+	cols = floor(width / w);
+	rows = floor(height / w);
+	grid = make2DArray(cols, rows);
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++){
+			grid[i][j] = new Cell(i, j, w);
+		}
+	}
+
+	let options = [];
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++) {
+			options.push([i, j]);
+		}
+	}
+
+	for (let i = 0; i < totalBees; i++) {
+		let index = floor(random(options.length));
+		let choice = options[index];
+		let i = choice[0];
+		let j = choice[1];
+		options.splice(index, 1);
+		grid[i][j].bee = true
+	}
+
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++){
+			grid[i][j].countBees();
+		}
+	}
+}
+
+function gameOver() {
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++){
+			grid[i][j].revealed = true;
+		}
+	}
+}
+
+function mousePressed() {
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++){
+			if (grid[i][j].contains(mouseX, mouseY)) {
+				grid[i][j].reveal();
+				if (grid[i][j].bee) {
+					gameOver();
+				}
+			}
+		}
+	}
+}
+
+function draw() {
+	background(255);
+
+	for (let i = 0; i < cols; i++) {
+		for (let j = 0; j < rows; j++){
+			grid[i][j].show();
+		}
+	}
+}
